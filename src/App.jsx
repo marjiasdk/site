4bc9237e15a680b6d0699d3cb6d56e00f7497c05
Marjia Siddik <marjia.siddik3@mail.dcu.ie>
2024-02-04 20:29:25 +0000

    personal changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Marjia Siddik",
+  title: "Computer Science Student",
+  email: "marjiasdk@gmail.com",
+  gitHub: "marjiasdk",
+  linkedIn: "marjiasdk",
 };
 
 const primaryColor = "#4E567E";
